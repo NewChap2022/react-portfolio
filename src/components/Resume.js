32a0e0b1b90e384b3d82c85feb42e96b974f3d94
NewChap2022 <irisyangshu@gmail.com>
2022-09-11 09:30:07 -0400

    fix bug on resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,24 +7,24 @@ const Resume = () => {
             <h2>Download My <a href={resume} download>Resume</a></h2>
             <div className="list-group">
                 <div>
-                    <h5 class="mb-1">Front-end Proficiencies</h5>
+                    <h5 className="mb-1">Front-end Proficiencies</h5>
                     <ul>
-                    <li class="mb-1">HTML</li>
-                    <li class="mb-1">CSS</li>
-                    <li class="mb-1">Javascript</li>
-                    <li class="mb-1">React</li>
-                    <li class="mb-1">Bootstrap</li>
-                    <li class="mb-1">Responsive Design</li>
+                    <li className="mb-1">HTML</li>
+                    <li className="mb-1">CSS</li>
+                    <li className="mb-1">Javascript</li>
+                    <li className="mb-1">React</li>
+                    <li className="mb-1">Bootstrap</li>
+                    <li className="mb-1">Responsive Design</li>
                     </ul>
                 </div>
                 <div>
-                    <h5 class="mb-1">Back-end Proficiencies</h5>
+                    <h5 className="mb-1">Back-end Proficiencies</h5>
                     <ul>
-                    <li class="mb-1">APIs</li>
-                    <li class="mb-1">MySQL Sequelize</li>
-                    <li class="mb-1">MongoDB</li>
-                    <li class="mb-1">Node</li>
-                    <li class="mb-1">Express</li>
+                    <li className="mb-1">APIs</li>
+                    <li className="mb-1">MySQL Sequelize</li>
+                    <li className="mb-1">MongoDB</li>
+                    <li className="mb-1">Node</li>
+                    <li className="mb-1">Express</li>
                     </ul>
                 </div>
             </div>
